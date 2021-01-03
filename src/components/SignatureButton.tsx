b90afcd8065ba50c1ed62af2949711eaf73bccda
Jack Hedaya <jackehedaya@gmail.com>
2021-01-03 17:43:37 -0500

    Added type property

--- src/components/SignatureButton.tsx
+++ src/components/SignatureButton.tsx
@@ -3,11 +3,17 @@ import s from '../styles/components/signature-button.module.scss'
 type SignatureButtonProps = {
   onClick?: () => void
   children?: React.ReactNode
+
+  /** Defaults to outlined */
+  type?: 'fill' | 'outlined'
 }
 
 export default function SignatureButton(props: SignatureButtonProps) {
   return (
-    <div className={s.button} onClick={props.onClick}>
+    <div
+      className={`${s.button} ${s[props.type || 'outlined']}`}
+      onClick={props.onClick}
+    >
       {props.children}
     </div>
   )
