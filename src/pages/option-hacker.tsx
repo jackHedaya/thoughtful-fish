8e01a8ccb234c421a45ef8ed036a4109aaba3628
Jack Hedaya <jackehedaya@gmail.com>
2021-01-03 16:50:55 -0500

    Setup option-hacker page

---
+++ src/pages/option-hacker.tsx
@@ -0,0 +1,9 @@
+import s from '../styles/pages/option-hacker.module.scss'
+
+export default function OptionHacker() {
+  return (
+    <div className="content">
+      <div className="page-title">Option Hacker</div>
+    </div>
+  )
+}
