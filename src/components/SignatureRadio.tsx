8465ea864f7d14957399998e4a4871f18104f069
Jack Hedaya <jackehedaya@gmail.com>
2021-01-03 17:43:55 -0500

    Created Signature Radio component

---
+++ src/components/SignatureRadio.tsx
@@ -0,0 +1,24 @@
+import s from '../styles/components/signature-radio.module.scss'
+import SignatureButton from './SignatureButton'
+
+type SignatureRadioProps = {
+  selectedElement: string
+  onSelect: (s: string) => void
+  items: string[]
+}
+
+export default function SignatureRadio(props: SignatureRadioProps) {
+  return (
+    <div className={s.radio}>
+      {props.items.map((item) => (
+        <SignatureButton
+          key={`SignatureRadio/${item}`}
+          onClick={() => props.onSelect(item)}
+          type={props.selectedElement === item ? 'fill' : 'outlined'}
+        >
+          {item}
+        </SignatureButton>
+      ))}
+    </div>
+  )
+}
