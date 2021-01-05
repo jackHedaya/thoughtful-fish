74be81941feb3fc3cb638e71519b8dc1a2db5341
Jack Hedaya <jackehedaya@gmail.com>
2021-01-05 16:20:04 -0500

    Wrapped with div

--- src/components/SignatureButton.tsx
+++ src/components/SignatureButton.tsx
@@ -1,20 +1,24 @@
+import { CSSProperties } from 'react'
 import s from '../styles/components/signature-button.module.scss'
 
 type SignatureButtonProps = {
   onClick?: () => void
   children?: React.ReactNode
-
   /** Defaults to outlined */
   type?: 'fill' | 'outlined'
+  style?: CSSProperties
+  className?: string
 }
 
 export default function SignatureButton(props: SignatureButtonProps) {
   return (
-    <div
-      className={`${s.button} ${s[props.type || 'outlined']}`}
-      onClick={props.onClick}
-    >
-      {props.children}
+    <div style={props.style} className={props.className || ''}>
+      <div
+        className={`${s.button}`}
+        onClick={props.onClick}
+      >
+        {props.children}
+      </div>
     </div>
   )
 }
