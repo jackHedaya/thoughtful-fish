55145fd023735ff20b95c4d67e9e6d8fb703053f
Jack Hedaya <jackehedaya@gmail.com>
2021-01-05 16:28:15 -0500

    Added list textfield option

--- src/components/HackerPresets/ExpressionPreset.tsx
+++ src/components/HackerPresets/ExpressionPreset.tsx
@@ -6,9 +6,13 @@ import SignatureButton from '../SignatureButton'
 import SignatureSelect from '../SignatureSelect'
 
 import s from '../../styles/components/hacker-presets/expression-preset.module.scss'
+import { TextField } from '@material-ui/core'
 
 export function ExpressionPreset() {
-  const [watchlistOrList, setWatchlistOrList] = useState('Watchlist')
+  const [watchlistOrList, setWatchlistOrList] = useState<'Watchlist' | 'List'>(
+    'Watchlist'
+  )
+  const [tickerList, setTickerList] = useState<string[]>([])
 
   return (
     <div>
@@ -21,15 +25,38 @@ export function ExpressionPreset() {
             onChange={(e) => setWatchlistOrList(e)}
             value={watchlistOrList}
           />
-          <SignatureAutocomplete
-            className={s.selectWatchlist}
-            title="Select Watchlist"
-            options={[
-              { group: "jackehedaya's Watchlists", value: 'Hello' },
-              { group: "jackehedaya's Watchlists", value: 'World' },
-              { group: "john's Watchlists", value: 'World' },
-            ]}
-          />
+          {watchlistOrList === 'Watchlist' ? (
+            <SignatureAutocomplete
+              className={s.selectWatchlist}
+              title="Select Watchlist"
+              options={[
+                { group: "jackehedaya's Watchlists", value: 'Hello' },
+                { group: "jackehedaya's Watchlists", value: 'World' },
+                { group: "john's Watchlists", value: 'World' },
+              ]}
+            />
+          ) : (
+            <TextField
+              className={s.selectWatchlist}
+              value={tickerList.join(',')}
+              onChange={(e) =>
+                setTickerList(
+                  e.currentTarget.value
+                    .split(',')
+                    .map((x) => x.trim().toUpperCase())
+                )
+              }
+              label="Ticker List"
+              placeholder="Comma seperated list"
+              variant="outlined"
+              inputProps={{
+                autocomplete: 'new-password',
+                form: {
+                  autocomplete: 'off',
+                },
+              }}
+            />
+          )}
         </div>
         <SignatureButton className={s.nextButton}>Next</SignatureButton>
       </SignatureAccordion>
