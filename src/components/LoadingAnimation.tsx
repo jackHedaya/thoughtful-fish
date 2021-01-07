a2a2fb23c4bd0fa21d0ebc4b42248368c594d37a
Jack Hedaya <jackehedaya@gmail.com>
2021-01-07 16:49:29 -0500

    Added loading animation component

---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,11 @@
+import s from '../styles/components/loading-animation.module.scss'
+
+export default function Loading() {
+  return (
+    <div className={s.facebookLoader}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
