8344f6dbedd8734c5ffabcaa45e2bfdd73d7caa7
Jack Hedaya <jackehedaya@gmail.com>
2021-01-09 23:18:28 -0500

    Ported to TypeScript

--- src/components/BubbleBackground/core.ts
+++ src/components/BubbleBackground/core.ts
@@ -1,5 +1,20 @@
-class bubble {
-  constructor(canvasWidth, canvasHeight, color) {
+class Bubble {
+  maxHeight: number
+  maxWidth: number
+
+  color: string
+
+  timeout: NodeJS.Timeout
+
+  posX: number
+  posY: number
+
+  movementX: any
+  movementY: any
+
+  size: number
+
+  constructor(canvasWidth: number, canvasHeight: number, color: string) {
     this.maxHeight = canvasHeight
     this.maxWidth = canvasWidth
     this.color = color
@@ -8,11 +23,11 @@ class bubble {
     this.timeout = null
   }
 
-  generateDecimalBetween(min, max) {
-    return (Math.random() * (min - max) + max).toFixed(2)
+  generateDecimalBetween(min: number, max: number) {
+    return Math.random() * (min - max) + max
   }
 
-  generateIntegerBetween(min, max) {
+  generateIntegerBetween(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
@@ -20,12 +35,15 @@ class bubble {
     this.posX = this.posX - this.movementX
     this.posY = this.posY - this.movementY
 
-    if (this.posY < 0 || this.posX < 0 || this.posX > this.maxWidth) {
+    // this.maxWidth = canvasWidth
+    // this.maxHeight = canvasHeight
+
+    if (this.posY < -100 || this.posX < 0 || this.posX > this.maxWidth + 100) {
       if (this.timeout) return
 
       this.timeout = setTimeout(() => {
         this.randomise()
-        this.posY = this.maxHeight
+        this.posY = this.maxHeight + 100
 
         this.timeout = null
       }, this.generateIntegerBetween(1000, 2000))
@@ -35,22 +53,32 @@ class bubble {
   randomise() {
     // this.color = Math.random() * 255;
     // this.color = "0,0,0";
-    this.size = this.generateDecimalBetween(30, 35)
-    this.movementX = this.generateDecimalBetween(-0.4, 0.4)
-    this.movementY = this.generateDecimalBetween(0.7, 2)
+    this.size = this.generateDecimalBetween(50, 100)
+    this.movementX = this.generateDecimalBetween(-0.4, 0.2)
+    this.movementY = this.generateDecimalBetween(0.7, 1.5)
     this.posX = this.generateDecimalBetween(0, this.maxWidth)
     this.posY = this.generateDecimalBetween(0, this.maxHeight)
   }
 }
 
-class background {
-  constructor(id, color) {
-    this.canvas = document.getElementById(id)
+export default class Background {
+  canvas: HTMLCanvasElement
+  ctx: CanvasRenderingContext2D
+  color: string
+  bubblesList: Bubble[]
+
+  constructor(id: string, color: string) {
+    this.canvas = document.getElementById(id) as HTMLCanvasElement
+
+    if (this.canvas === null)
+      throw `Unable to find canvas with id ${id}`
+
     this.ctx = this.canvas.getContext('2d')
     this.canvas.height = window.innerHeight
     this.canvas.width = window.innerWidth
     this.color = color
     this.bubblesList = []
+
     this.generateBubbles()
     this.animate()
   }
@@ -60,11 +88,13 @@ class background {
     self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height)
     self.bubblesList.forEach(function (bubble) {
       bubble.update()
+
       self.ctx.beginPath()
       self.ctx.arc(bubble.posX, bubble.posY, bubble.size, 0, 2 * Math.PI)
       self.ctx.fillStyle = bubble.color
       self.ctx.globalAlpha = 0.39
       self.ctx.fill()
+
       self.ctx.strokeStyle = bubble.color
       self.ctx.stroke()
     })
@@ -72,7 +102,7 @@ class background {
     requestAnimationFrame(this.animate.bind(this))
   }
 
-  addBubble(bubble) {
+  addBubble(bubble: Bubble) {
     return this.bubblesList.push(bubble)
   }
 
@@ -80,7 +110,7 @@ class background {
     let self = this
     for (let i = 0; i < self.bubbleDensity(); i++) {
       self.addBubble(
-        new bubble(self.canvas.width, self.canvas.height, self.color)
+        new Bubble(self.canvas.width, self.canvas.height, self.color)
       )
     }
   }
@@ -89,20 +119,3 @@ class background {
     return Math.sqrt((this.canvas.height, this.canvas.width) / 6)
   }
 }
-
-export default function init(id, color) {
-  new background(id, color)
-}
-
-window.requestAnimFrame = (function () {
-  return (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function (callback) {
-      window.setTimeout(callback, 1000 / 60)
-    }
-  )
-})()
