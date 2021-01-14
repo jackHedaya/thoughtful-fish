705d96a00c89a59ce0632be640092b86e5e02c7d
Jack Hedaya <jackehedaya@gmail.com>
2021-01-14 17:56:06 -0500

    Added auth module to help with cookie manipulation

---
+++ src/lib/thoughtful-fish/auth.ts
@@ -0,0 +1,99 @@
+import { NextApiRequest, NextApiResponse, NextPageContext } from 'next'
+import { parseCookies, setCookie } from 'nookies'
+import jwt from 'jsonwebtoken'
+
+declare module 'http' {
+  interface IncomingMessage {
+    _cookies?: { [key: string]: string }
+  }
+}
+
+type DecodedToken = { [key: string]: string }
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+type WriteAccessParams = {
+  res: NextApiResponse
+  accessToken: string
+  expiresIn: number
+}
+
+export function writeAccessToken(params: WriteAccessParams) {
+  const { res, accessToken, expiresIn } = params
+
+  return writeToken({ res, key: 'accessToken', token: accessToken, expiresIn })
+}
+
+type WriteRefreshParams = {
+  res: NextApiResponse
+  refreshToken: string
+  expiresIn: number
+}
+
+export function writeRefreshToken(params: WriteRefreshParams) {
+  const { res, refreshToken, expiresIn } = params
+
+  return writeToken({
+    res,
+    key: 'refreshToken',
+    token: refreshToken,
+    expiresIn,
+  })
+}
+
+type WriteTokenParams = {
+  res: NextApiResponse
+  key: string
+  token: string
+  expiresIn: number
+}
+
+function writeToken(params: WriteTokenParams) {
+  const { res, key, token, expiresIn } = params
+
+  const tokenJwt = jwt.sign({ token }, JWT_SECRET, {
+    expiresIn: expiresIn,
+  })
+
+  setCookie({ res }, key, tokenJwt, {
+    httpOnly: true,
+    path: '/',
+    maxAge: expiresIn,
+  })
+}
+
+type ContextOrRequest = NextPageContext | NextApiRequest
+
+export function getRefreshToken(ctxOrReq: ContextOrRequest) {
+  return getToken({ ctxOrReq, key: 'refreshToken' })
+}
+
+export function getAccessToken(ctxOrReq: ContextOrRequest) {
+  return getToken({ ctxOrReq, key: 'accessToken' })
+}
+
+type GetTokenParams = {
+  ctxOrReq: ContextOrRequest
+  key: string
+}
+
+function getToken({ ctxOrReq, key }: GetTokenParams) {
+  try {
+    const req = !isCtx(ctxOrReq) ? ctxOrReq : ctxOrReq.req
+
+    if (!req._cookies) req._cookies = parseCookies({ req })
+
+    let tokenJwt = req._cookies[key]
+
+    const { token } = jwt.verify(tokenJwt, JWT_SECRET) as DecodedToken
+
+    return token || null
+  } catch (e) {
+    return null
+  }
+}
+
+function isCtx(x: any): x is NextPageContext {
+  if (!!x.req) return true
+  return false
+}
