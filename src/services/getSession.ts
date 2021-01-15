822c7d17f423e0b70f97294bd3311d6d1278d7b2
Jack Hedaya <jackehedaya@gmail.com>
2021-01-14 20:59:21 -0500

    Have session include Profile

--- src/services/getSession.ts
+++ src/services/getSession.ts
@@ -1,12 +1,15 @@
 import { NextPageContext } from 'next'
+import ameritrade from '../lib/ameritrade'
 
 import { getAccessToken, getRefreshToken } from '../lib/thoughtful-fish/auth'
 
-export default function getSession(ctx: NextPageContext): Session {
+export default async function getSession(ctx: NextPageContext): Promise<Session> {
   const accessToken = getAccessToken(ctx)
   const refreshToken = getRefreshToken(ctx)
 
   if (!accessToken || !refreshToken) return null
 
-  return { accessToken, refreshToken }
+  const profile: Profile = await ameritrade.account.getProfile({ accessToken }).catch(() => null)
+
+  return { accessToken, refreshToken, profile }
 }
