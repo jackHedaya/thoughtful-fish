7689f7342cc870b7a39cefc66b08f07b00fa1f20
Jack Hedaya <jackehedaya@gmail.com>
2021-01-18 00:49:12 -0400

    Removed unnecessary call

--- src/pages/option-hacker/index.tsx
+++ src/pages/option-hacker/index.tsx
@@ -67,7 +67,6 @@ export default function OptionHacker() {
                 onComplete={(d: PresetState) => {
                   setSendData(cleanState({ preset, ...d }))
                   setWillTransition(true)
-                  getResults()
                 }}
               />
             </div>
