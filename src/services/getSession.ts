06d794dc1ccf59763be67920adb064aa374a665e
Jack Hedaya <jackehedaya@gmail.com>
2021-01-17 22:41:49 -0400

    Accept request as parameter

--- src/services/getSession.ts
+++ src/services/getSession.ts
@@ -1,8 +1,6 @@
-import { NextPageContext } from 'next'
-
 import { getAccessToken, getProfile, getRefreshToken } from '../lib/thoughtful-fish/session'
 
-export default function getSession(ctx: NextPageContext): Session {
+export default function getSession(ctx: ContextOrRequest): Session {
   const accessToken = getAccessToken(ctx)
   const refreshToken = getRefreshToken(ctx)
   const profile = getProfile(ctx)
