e34ad7b5b765b32b3ff61c9dc972c17bdffcdcd4
Jack Hedaya <jackehedaya@gmail.com>
2021-02-10 10:26:20 -1000

    Added sorted import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,11 @@ module.exports = {
   globals: {
     React: 'writeable',
   },
+  plugins: ['import'],
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:import/typescript',
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
@@ -27,5 +29,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/no-unescaped-entities': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/order': ['warn', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+    'sort-imports': ['warn', { ignoreDeclarationSort: true, ignoreCase: true }],
   },
 }
