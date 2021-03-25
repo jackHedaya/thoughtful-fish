d756f7f2229e8a4ca275df748d2f048511f3d917
Jack Hedaya <jackehedaya@gmail.com>
2021-03-25 16:39:42 -0400

    Changed usePrettyLoading to accept isLoading and consolidate condition

--- src/hooks/usePrettyLoading.ts
+++ src/hooks/usePrettyLoading.ts
@@ -4,14 +4,14 @@ import { useEffect, useState } from 'react'
  * A hook to prevent a loading flash for loading states
  * @param milliseconds Milliseconds to load
  */
-export default function usePrettyLoading(milliseconds: number) {
-  const [isLoading, setIsLoading] = useState(true)
+export default function usePrettyLoading(isLoading: boolean, milliseconds: number) {
+  const [isTimedOut, setIsTimedOut] = useState(true)
 
   useEffect(() => {
-    const s = setTimeout(() => setIsLoading(false), milliseconds)
+    const s = setTimeout(() => setIsTimedOut(false), milliseconds)
 
     return () => clearTimeout(s)
   }, [milliseconds])
 
-  return isLoading
+  return isLoading && isTimedOut
 }
